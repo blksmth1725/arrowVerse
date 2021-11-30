2773658d32559564621b64fedfddff8cb36a811e
Christian Sheen <csheen1725@gmail.com>
2021-11-29 21:49:52 -0500

    Boilerplate clean-up && installed ChakraUI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//React
+import React from "react";
+import ReactDOM from "react-dom";
+
+//App
+import App from "./App";
+
+// Styling
+import { appTheme } from "./utils/theme";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={appTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
