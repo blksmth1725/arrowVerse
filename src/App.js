571fafab4b24b03d79933a6cd17f0c5c231d174f
Christian Sheen <csheen1725@gmail.com>
2021-12-13 16:46:01 -0500

    Build Navbar to navigate through app

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import "./App.css";
 import MainPage from "./components/MainPage";
 import CastMemberPage from "./components/CastMemberPage";
 import EpisodesPage from "./components/EpisodesPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@chakra-ui/layout";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Box>Navbar</Box>
+    </div>
+  );
+};
+
+export default Navbar;
